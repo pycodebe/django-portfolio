c30fc42a15141bf3c36074f8daf4499e3176e4cb
Olivier <noreply4github@gmail.com>
2022-04-30 17:13:55 +0200

    feature: add some bootstrap style to my forms

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('projects/', include('projects.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- users/views.py
+++ users/views.py
@@ -7,14 +7,14 @@ def dashboard(request):
     return render(request, 'users/dashboard.html')
 
 def register(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         return render(
-            request, "users/register.html",
-            {"form": CustomUserCreationForm}
+            request, 'users/register.html',
+            {'form': CustomUserCreationForm}
         )
-    elif request.method == "POST":
+    elif request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(reverse("dashboard"))
+        return render(request, 'core/dashboard.html')
