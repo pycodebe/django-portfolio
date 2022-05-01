1425ce60570e3309ffb317e74a6f596368e16c0a
Olivier <noreply4github@gmail.com>
2022-05-01 09:53:57 +0200

    some refactoring in the code

--- users/views.py
+++ users/views.py
@@ -1,21 +1,27 @@
 from django.contrib.auth import login
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 from users.forms import CustomUserCreationForm
+from django.contrib import messages
 
+
+@login_required
+@user_passes_test(lambda user: user.is_authenticated)
 def dashboard(request):
-    return render(request, 'users/dashboard.html')
+    user = request.user
+    return render(request, template_name='users/dashboard.html', context={
+        'username': user.username,
+        'is_staff': user.is_staff,
+        'is_admin': user.is_admin
+        })
+
 
 def register(request):
-    if request.method == 'GET':
-        return render(
-            request, 'users/register.html',
-            {'form': CustomUserCreationForm}
-        )
-    elif request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.backend = 'django.contrib.auth.backends.ModelBackend'
-            login(request, user)
-        return render(request, 'core/dashboard.html')
+    form = CustomUserCreationForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = form.save()
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(request, user)
+        messages.success(request, 'Registration successful.')
+        return render(request, template_name='users/dashboard.html', )
+    return render(request=request, template_name="users/register.html", context={'form':form})
